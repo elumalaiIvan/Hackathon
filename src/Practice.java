d610800c5f7034c4d139314bfcee38a24a5fd413
elumalai <mynameiselumalai@gmail.com>
2022-10-18 18:27:38 +0530

    measure closure added.

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,18 @@
+import java.util.function.Function;
+
 public class Practice {
     public static void main(String[] args) {
+       measure((Void) -> {
+           int maxSumPossible = new MaxSumPossible().findMaxSumPossibleApproach3(new int[]{100, 200, 300, 400}, 2);
+           System.out.println("maximum sum is: " + maxSumPossible);
+           return null;
+       });
+    }
+
+    static void measure(Function<Void, Void> closure) {
         long start2 = System.currentTimeMillis();
-        int maxSumPossible = new MaxSumPossible().findMaxSumPossibleApproach1(new int[]{100, 200, 300, 400}, 2);
+        closure.apply(null);
         long end2 = System.currentTimeMillis();
         System.out.println("Elapsed Time in milli seconds: "+ (end2-start2));
-        System.out.println("maximum sum is: " + maxSumPossible);
     }
 }
