45e20bf248ffa9a9c6ecff136d712882ad745ac3
elumalai <mynameiselumalai@gmail.com>
2023-08-01 15:20:13 -0400

    updated sorted merge, with multiple other ways

--- src/SortedMerge.java
+++ src/SortedMerge.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class SortedMerge {
     public static void main(String[] args) {
-//        int[] firstArray = {1,2,6,12,13};
+        int[] firstArray = {1,2,6,12,13};
         int[] secondArray = {5,10};
-
         System.out.println(Arrays.toString(sortedMerge(new int[]{1,2,6,12,13}, secondArray)));
+        System.out.println(Arrays.toString(sortedMergeWithArraycopy(firstArray, secondArray)));
+        System.out.println(Arrays.toString(sortedMergeWithPriorityQueue(firstArray, secondArray)));
     }
 
+    //time complexity ->
     private static int[] sortedMerge(int[] firstArray, int[] secondArray) {
         int firstPointer = 0;
         int secondPointer = 0;
@@ -43,6 +46,44 @@ public class SortedMerge {
         }
 
         return mergedArray;
+    }
+
+    //complexity -> The time complexity of this approach is O((m + n) log(m + n)), where m and n are the sizes of the two arrays.
+
+    public static int[] sortedMergeWithPriorityQueue(int[] firstArray, int[] secondArray) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int m = firstArray.length;
+        int n = secondArray.length;
+        int[] mergedArray = new int[m + n];
+
+        // Add all elements from both arrays to the minHeap
+        for (int num : firstArray) {
+            minHeap.add(num);
+        }
+        for (int num : secondArray) {
+            minHeap.add(num);
+        }
+
+        // Retrieve elements from minHeap and put them into the mergedArray
+        int k = 0;
+        while (!minHeap.isEmpty()) {
+            mergedArray[k++] = minHeap.poll();
+        }
+
+        return mergedArray;
+
+    }
+
+    //complexity - O((m + n) log(m + n)), where m and n are the sizes of the two arrays.
+    public static int[] sortedMergeWithArraycopy(int[] firstArray, int[] secondArray) {
+        int capacity = firstArray.length + secondArray.length;
+        int[] mergedArray = new int[capacity];
+
+        System.arraycopy(firstArray, 0, mergedArray, 0, firstArray.length);
+        System.arraycopy(secondArray, 0, mergedArray, firstArray.length, secondArray.length);
+
+        return Arrays.stream(mergedArray).sorted().toArray();
+
     }
 
 }
